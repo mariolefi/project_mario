f5955b30f9e1ca464740f934b46b48956085f965
mariolefi <marioaldo763@gmail.com>
2025-02-10 11:24:55 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+// localhost:8000/admin/inputbuku
+Route::get('/admin/inputbuku', function () {
+    return view('admin.inputbuku');
+});
+
+// route untuk kategori
+// Route::get('/admin/kategori', function () {
+//     return view('admin.kategori');
+// });
+Route::get('/admin/inputkategori', [CategoryController::class, 'index'])->name('admin.inkategori');
+
+// route untuk penulis
+Route::get('/admin/penulis', function () {
+    return view('admin.penulis');
 });
